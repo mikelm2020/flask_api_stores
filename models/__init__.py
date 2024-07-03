baff7e293a27f0c9e9850e3c12960f6bfcb66082
Miguel Angel <miguel.lopezm.dev@gmail.com>
2024-07-03 17:00:06 -0600

    Initial commit of the project

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from models.item import ItemModel  # noqa: F401
+from models.item_tags import ItemsTags  # noqa: F401
+from models.store import StoreModel  # noqa: F401
+from models.tag import TagModel  # noqa: F401
+from models.user import UserModel  # noqa: F401
---
+++ resources/tag.py
@@ -0,0 +1,106 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import ItemModel, StoreModel, TagModel
+from schemas import TagAndItemSchema, TagSchema
+
+blp = Blueprint("Tags", "tags", description="Operations on tags")
+
+
+@blp.route("/store/<int:store_id>/tag")
+class TagsInStore(MethodView):
+    @blp.response(200, TagSchema(many=True))
+    def get(self, store_id):
+        store = StoreModel.query.get_or_404(store_id)
+
+        return store.tags.all()
+
+    @blp.arguments(TagSchema)
+    @blp.response(201, TagSchema)
+    def post(self, tag_data, store_id):
+        if TagModel.query.filter(
+            TagModel.store_id == store_id, TagModel.name == tag_data["name"]
+        ).first():
+            abort(400, message="A tag with that name already exists in that store.")
+
+        tag = TagModel(**tag_data, store_id=store_id)
+
+        try:
+            db.session.add(tag)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            abort(
+                500,
+                message=str(e),
+            )
+
+        return tag
+
+
+@blp.route("/item/<int:item_id>/tag/<int:tag_id>")
+class LinkTagToItem(MethodView):
+    @blp.response(200, TagSchema)
+    def post(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.append(tag)
+        try:
+            if item.store.id != tag.store.id:
+                abort(
+                    400,
+                    message="Make sure item and tag belong to the same store before linking.",
+                )
+
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the tag.")
+
+        return tag
+
+    @blp.response(200, TagAndItemSchema)
+    def delete(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.remove(tag)
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the tag.")
+
+        return {"message": "Item removed from tag", "item": item, "tag": tag}
+
+
+@blp.route("/tag/<int:tag_id>")
+class Tag(MethodView):
+    @blp.response(200, TagSchema)
+    def get(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+        return tag
+
+    @blp.response(
+        202,
+        description="Delete a tag if no item is tagged with it",
+        example={"message": "Tag deleted."},
+    )
+    @blp.response(404, description="Tag not found.")
+    @blp.alt_response(
+        400,
+        description="REturned if tag is assigned to one o more items. In this case, the tag is not deleted.",
+    )
+    def delete(self, tag_id):
+        tag = TagModel.query.get_or_404(tag_id)
+
+        if not tag.items:
+            db.session.delete(tag)
+            db.session.commit()
+            return {"message": "Tag deleted."}
+        abort(
+            400,
+            message="Coud not delete tag. Make sure tag is not associated with any items, then try again.",
+        )
